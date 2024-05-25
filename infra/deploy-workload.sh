24cb13d969a14d7883f8543153f0e7a0ae02503e
Yathartha <varshneya.yathartha@gmail.com>
2024-05-25 11:39:30 +0530

    added steps for helm install

--- infra/deploy-workload.sh
+++ infra/deploy-workload.sh
@@ -12,3 +12,19 @@ kubectl apply -f ../manifests/be-svc.yaml
 
 # Deploy ingress
 kubectl apply -f ../manifests/ingress.yaml
+
+
+# ---------------- USING HELM ------------------------------------------
+
+# Install helm - https://helm.sh/docs/intro/install/ 
+
+# Pull the packaged chart
+    helm pull bitnami/mysql 
+    # might need more steps to pull from my repository https://docs.google.com/document/d/1dyYB0SRvaKGH2FGy_zVteEeXq3CDweth-4WqwOqOoC4/edit#heading=h.mcxdgjajqcou
+
+# Install a package/chart
+    helm upgrade --install --namespace ns2 mydb bitnami/mysql --values ./values.yaml
+
+
+
+
