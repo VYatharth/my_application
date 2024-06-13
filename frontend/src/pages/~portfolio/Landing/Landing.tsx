c692935e462a0be8fc4b5ee5632d52d24916b4a3
Yathartha <varshneya.yathartha@gmail.com>
2024-06-13 11:04:25 +0530

    Added landing component

--- frontend/src/pages/~portfolio/Landing/Landing.tsx
+++ frontend/src/pages/~portfolio/Landing/Landing.tsx
@@ -2,72 +2,99 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { headerData } from '../../../data/headerData';
 import { socialsData } from '../../../data/socialsData';
 import { Link } from '@tanstack/react-router';
-
+import AuthorImage from "../../../assets/images/blog-author.jpg"
+import { BiLogoFacebookSquare, BiLogoTwitter, BiLogoInstagram, BiLogoLinkedin, BiLogoLinkedinSquare } from "react-icons/bi";
 const Landing = () => {
 
     return (
-        <div className="landing h-screen flex items-center justify-center">
-            <div className="landing--container flex items-center justify-center h-full relative">
-                <div
-                    className="landing--container-left flex-35 h-full flex items-end justify-start"
-                    style={{ backgroundColor: "ui.main" }}
-                >
-                    <div className="lcl--content m-12 w-full flex items-center justify-start">
-                        {socialsData.linkedIn && (
-                            <a
-                                href={socialsData.linkedIn}
-                                target='_blank'
-                                rel='noreferrer'
+        <>
+            <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-[110vh]"
+            >
+                <div className="absolute top-0 w-full h-full bg-center bg-cover"
+                    style={{
+                        backgroundImage: "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')"
+                    }}>
+                    <span id="blackOverlay" className="w-full h-full absolute opacity-75 bg-black"></span>
+                </div>
+                <div className="container relative mx-auto">
+                    <div className="items-center flex flex-wrap">
+                        <div className="w-full lg:w-10/12 px-4 ml-auto mr-auto text-center">
+                            <div
+                                className="container relative z-30  pb-12 sm:pt-56 sm:pb-48 lg:pt-64 lg:pb-48"
                             >
-                                <FaLinkedin
-                                    className='landing--social text-2xl m-4 transition-transform duration-500 hover:scale-125'
-                                    style={{ color: "ui.secondary" }}
-                                    aria-label='LinkedIn'
-                                />
-                            </a>
-                        )}
-                        {socialsData.github && (
-                            <a
-                                href={socialsData.github}
-                                target='_blank'
-                                rel='noreferrer'
-                            >
-                                <FaGithub
-                                    className='landing--social text-2xl m-4 transition-transform duration-500 hover:scale-125'
-                                    style={{ color: "ui.secondary" }}
-                                    aria-label='GitHub'
-                                />
-                            </a>
-                        )}
+                                <div className="flex flex-col items-center justify-center lg:flex-row">
+                                    <div className="rounded-full border-8 border-primary shadow-xl">
+                                        <img
+                                            src={AuthorImage}
+                                            className="h-48 rounded-full sm:h-56"
+                                            alt="author"
+                                        />
+                                    </div>
+                                    <div className="pt-8 sm:pt-10 lg:pl-8 lg:pt-0">
+                                        <h1
+                                            className="text-center font-header text-4xl text-white sm:text-left sm:text-5xl md:text-6xl"
+                                        >
+                                            Hello I'm Christy Smith!
+                                        </h1>
+                                        <div
+                                            className="flex flex-col justify-center pt-3 sm:flex-row sm:pt-5 lg:justify-start"
+                                        >
+                                            <div
+                                                className="flex items-center justify-center pl-0 sm:justify-start md:pl-1"
+                                            >
+                                                <p className="font-body text-lg uppercase text-white">Let's connect</p>
+                                                <div className="hidden sm:block">
+                                                    <i className="bx bx-chevron-right text-3xl text-yellow"></i>
+                                                </div>
+                                            </div>
+                                            <div
+                                                className="flex items-center justify-center pt-5 pl-2 sm:justify-start sm:pt-0"
+                                            >
+                                                <a href="/">
+                                                    <BiLogoFacebookSquare className="text-2xl text-white hover:text-yellow-600" />
+                                                </a>
+                                                <a href="/" className="pl-4">
+                                                <BiLogoTwitter className="text-2xl text-white hover:text-yellow-600" />
+                                                   
+                                                </a>
+                                                <a href="/" className="pl-4">
+                                              
+                                                <BiLogoLinkedinSquare className="text-2xl text-white hover:text-yellow-600" />
+                                                </a>
+                                                <a href="/" className="pl-4">
+                                                <BiLogoInstagram className="text-2xl text-white hover:text-yellow-600" />
+                                                 
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
-                <img
-                    src={headerData.image}
-                    alt=''
-                    className='landing--img absolute left-1/2 transform -translate-x-1/2 w-80 h-80 object-cover rounded-full shadow-lg transition-opacity duration-300'
-                    style={
-                        {
-                            borderColor: "ui.secondary",
-                        }
-                    }
-                />
                 <div
-                    className='landing--container-right flex-65 h-full bg-gray-200 flex flex-col items-end justify-center'
-                    style={{ backgroundColor: "ui.secondary" }}
+                    className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
+                    style={{ height: "70px" }}
                 >
-                    <div
-                        className='lcr--content w-1/2 mr-28 font-sans'
-                        style={{ color: "ui.dim" }}
+                    <svg
+                        className="absolute bottom-0 overflow-hidden"
+                        xmlns="http://www.w3.org/2000/svg"
+                        preserveAspectRatio="none"
+                        version="1.1"
+                        viewBox="0 0 2560 100"
+                        x="0"
+                        y="0"
                     >
-                        <h6 className='text-lg font-medium mb-4 opacity-80'>{headerData.title}</h6>
-                        <h1 className='text-4xl font-semibold my-4'>{headerData.name}</h1>
-                        <p className='mt-4 text-lg font-medium opacity-70'>{headerData.desciption}</p>
-
-                       
-                    </div>
+                        <polygon
+                            className="text-gray-300 fill-current"
+                            points="2560 0 2560 100 0 100"
+                        ></polygon>
+                    </svg>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
