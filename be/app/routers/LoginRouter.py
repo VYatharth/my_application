d98c1a98a3d9c15ec1da44e28a0b21d89adb5f2e
Yathartha <varshneya.yathartha@gmail.com>
2024-06-30 18:31:13 +0530

    added gemini API call

--- be/app/routers/LoginRouter.py
+++ be/app/routers/LoginRouter.py
@@ -3,6 +3,11 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from app.schemas.LoginSchema import LoginRequestSchema
 from app.schemas.UserSchema import UserRequestSchema, UserResponseSchema
 from app.services.UserService import UserService
+import google.generativeai as genai
+import os
+
+
+
 
 LoginRouter = APIRouter(
   tags=['login']
@@ -11,4 +16,13 @@ LoginRouter = APIRouter(
 @LoginRouter.get("/health", response_model=bool)
 def index():
     return  True
+
+@LoginRouter.get("/models")
+def index():
+    key = os.getenv('API_KEY')
+    genai.configure(api_key=key)
+    
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    response = model.generate_content("Write a story about an elephant in 200 words")
+    return  response.text
 
