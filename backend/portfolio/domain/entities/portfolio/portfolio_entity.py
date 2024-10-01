39f932dc7d8a0cef3d9ea78bb49782e1ca611faa
Yathartha <varshneya.yathartha@gmail.com>
2024-10-01 08:31:30 +0530

    committing portfolio repo abstraction

--- backend/portfolio/domain/entities/portfolio/portfolio_entity.py
+++ backend/portfolio/domain/entities/portfolio/portfolio_entity.py
@@ -1,7 +1,11 @@
+from dataclasses import dataclass
+from typing import Any
+
+@dataclass
 class Portfolio_Entity:
-    id: int
-    author: str
-    category: str
-    content: str
-    created_at: datetime.datetime
-    updated_at: datetime.datetime
+    personal_details: Any
+    skills: Any
+    achievements: Any
+    experience: Any
+    activities: Any
+    contact_info: Any
--- backend/portfolio/domain/repository_interfaces/portfolio_repository.py
+++ backend/portfolio/domain/repository_interfaces/portfolio_repository.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 from portfolio.common.models.portfolio_dto import PortfolioDto
-from portfolio.domain.entities.portfolio.portfolio_entity import Portfolio_Entity
 
 
 class PortfolioRepository(ABC):
