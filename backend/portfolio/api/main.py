29c72554ec38e976ce8875f2282971e53f865d5e
Yathartha <varshneya.yathartha@gmail.com>
2024-10-12 03:13:44 +0530

    renamed main app

--- backend/portfolio/api/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from portfolio.api.routes import login, users, portfolio_route, scan_pdf_route
-
-api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(portfolio_route.router, prefix="/portfolio", tags=["portfolio"])
-api_router.include_router(scan_pdf_route.router, prefix="/pdf", tags=["pdf"])
