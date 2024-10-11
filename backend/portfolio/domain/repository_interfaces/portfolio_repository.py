44eabcc23be0879e2ce0ce1a618615bc195158d7
Yathartha <varshneya.yathartha@gmail.com>
2024-10-12 03:14:09 +0530

    rename portfolio to my_app

--- backend/portfolio/domain/repository_interfaces/portfolio_repository.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from portfolio.common.models.portfolio_dto import PortfolioDto
-
-
-class PortfolioRepository(ABC):
-    @abstractmethod
-    def get_portfolio(self) -> PortfolioDto:
-        pass
--- backend/portfolio/domain/usecases/read_portfolio_use_case.py
+++
@@ -1,6 +0,0 @@
-from portfolio.common.models.portfolio_dto import PortfolioDto
-from portfolio.domain.repository_interfaces.portfolio_repository import PortfolioRepository
-
-def read_portfolio_use_case(repo: PortfolioRepository ) -> PortfolioDto:
-    return repo.get_portfolio()
-    
