5e5991c39befa3dfd3da22fa091e6de933a1b9b7
Yathartha <varshneya.yathartha@gmail.com>
2024-10-12 20:47:24 +0530

    updated question route

--- backend/my_app/api/routes/question_route.py
+++ backend/my_app/api/routes/question_route.py
@@ -1,13 +1,12 @@
 from typing import Annotated
-from fastapi import APIRouter, Body, Depends, HTTPException, Query, Request
-
-from my_app.common.models.question_dto import ArticleDto
+from fastapi import APIRouter, Body, Depends, Form, HTTPException, Request
+from my_app.common.models.question_dto import QueryTextDto
 from my_app.domain.repository_interfaces.file_repository import FileRepository
 from my_app.domain.repository_interfaces.storage_repository import StorageRepository
-from my_app.domain.usecases.genai_use_cases import generate_content_use_case, process_file_use_case, process_text_use_case, query_document_use_case
+from my_app.domain.usecases.genai_use_cases import generate_content_use_case, process_text_use_case, query_document_use_case
 from my_app.domain.repository_interfaces.genai_repository import GenaiRepository
 from my_app.api.dependencies import get_file_repository, get_genai_repository, get_storage_repository
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter
 
 
 router = APIRouter()
@@ -20,11 +19,11 @@ def models(genai_repository: Annotated[GenaiRepository, Depends(get_genai_reposi
     
   
 @router.post("/processtext")
-def upload_files(article_dto: ArticleDto, request: Request, genai_repository: Annotated[GenaiRepository, Depends(get_genai_repository)], storage_repository: Annotated[StorageRepository, Depends(get_storage_repository)], file_repository: Annotated[FileRepository, Depends(get_file_repository)]) -> str:
-    print(article_dto.article)
-    if article_dto.article:
+def upload_files(article_dto: QueryTextDto, request: Request, genai_repository: Annotated[GenaiRepository, Depends(get_genai_repository)], storage_repository: Annotated[StorageRepository, Depends(get_storage_repository)], file_repository: Annotated[FileRepository, Depends(get_file_repository)]) -> str:
+    print(article_dto)
+    if article_dto.text_content:
         try:
-            process_text_use_case(article_dto.article, genai_repository, storage_repository, request.app.state.genai_key)
+            process_text_use_case(article_dto.text_content, genai_repository, storage_repository, request.app.state.genai_key)
         except:
             raise HTTPException(status_code=500, detail="Error in processing text")
     else:
@@ -32,11 +31,11 @@ def upload_files(article_dto: ArticleDto, request: Request, genai_repository: An
     return  'text processed successfully'
 
 
-@router.post("/question", response_model=str)
-def question(question: Annotated[str, Body()], request: Request, genai_repository: Annotated[GenaiRepository, Depends(get_genai_repository)], storage_repository: Annotated[StorageRepository, Depends(get_storage_repository)]) -> str :
-    if question:
+@router.post("/query", response_model=str)
+def query(query: Annotated[str, Body(embed=True)], request: Request, genai_repository: Annotated[GenaiRepository, Depends(get_genai_repository)], storage_repository: Annotated[StorageRepository, Depends(get_storage_repository)]) -> str :
+    if query:
         try:
-            result = query_document_use_case(genai_repository, storage_repository, question, request.app.state.genai_key) 
+            result = query_document_use_case(genai_repository, storage_repository, query, request.app.state.genai_key) 
         
             return result
         except:
