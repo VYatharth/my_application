8afd81a7117b5531fec8457b0e1f35482662e0d9
Yathartha <varshneya.yathartha@gmail.com>
2024-10-13 11:35:06 +0530

    cleaned up users

--- backend/my_app/api/main.py
+++ backend/my_app/api/main.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
-from my_app.api.routes import login, users, portfolio_route, question_route
+from my_app.api.routes import login, portfolio_route, question_route
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(portfolio_route.router, prefix="/portfolio", tags=["portfolio"])
 api_router.include_router(question_route.router, prefix="/question", tags=["question"])
 # api_router.include_router(scan_pdf_route.router, prefix="/pdf", tags=["pdf"])
