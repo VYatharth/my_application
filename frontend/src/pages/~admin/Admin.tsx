891da160824c3f619ef5f1fcada957ef3b0f1524
Yathartha <varshneya.yathartha@gmail.com>
2024-10-15 00:49:24 +0530

    updated admin component

--- frontend/src/pages/~admin/Admin.tsx
+++ frontend/src/pages/~admin/Admin.tsx
@@ -1,9 +1,5 @@
 const Admin = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  );
+  return <h1 className='text-3xl font-bold underline'>Admin Page</h1>;
 };
 
 export default Admin;
