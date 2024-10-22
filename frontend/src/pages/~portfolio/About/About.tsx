2395abf822386597ce876fb3bf6d15859f1cd1ef
Yathartha <varshneya.yathartha@gmail.com>
2024-10-23 01:48:08 +0530

    updated about and landing page

--- frontend/src/pages/~portfolio/About/About.tsx
+++ frontend/src/pages/~portfolio/About/About.tsx
@@ -1,74 +1,69 @@
-import { BiLogoLinkedinSquare } from "react-icons/bi";
 import AnimatedCard from '../../../components/AnimatedCard/AnimatedCard';
-import BulletEllipse from "../../../components/BulletEllipse/BulletEllipse";
+import BulletEllipse from '../../../components/BulletEllipse/BulletEllipse';
+import { portfolioData } from '../../../data/portfolioData';
 
 const About = () => {
+  return (
+    <section className='relative py-20 bg-gray-100 pb-44 border-b border-gray-900'>
+      <div
+        className='bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20'
+        style={{ height: '80px' }}
+      >
+        <svg
+          className='absolute bottom-0 overflow-hidden'
+          xmlns='http://www.w3.org/2000/svg'
+          preserveAspectRatio='none'
+          version='1.1'
+          viewBox='0 0 2560 100'
+          x='0'
+          y='0'
+        >
+          <polygon className='text-gray-100 fill-current' points='2560 0 2560 110 0 100'></polygon>
+        </svg>
+      </div>
 
-    return (
-        <section className="relative py-20 bg-gray-100 pb-44">
-            <div
-                className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
-                style={{ height: "80px" }}
-            >
-                <svg
-                    className="absolute bottom-0 overflow-hidden"
-                    xmlns="http://www.w3.org/2000/svg"
-                    preserveAspectRatio="none"
-                    version="1.1"
-                    viewBox="0 0 2560 100"
-                    x="0"
-                    y="0"
-                >
-                    <polygon
-                        className="text-gray-100 fill-current"
-                        points="2560 0 2560 100 0 100"
-                    ></polygon>
-                </svg>
+      <div className='mx-auto px-4 mt-10'>
+        <div className='items-center flex flex-wrap'>
+          <div className='w-full hidden lg:block w-[450px] xl:w-[480px] ml-auto mr-auto px-2'>
+            <div className='flex flex-wrap gap-6'>
+              {portfolioData.primarySkills.map((skill, index) => (
+                <AnimatedCard
+                  key={`title${index}`}
+                  index={index}
+                  title={skill.skill}
+                  description={skill.description}
+                  icon={skill.icon}
+                />
+              ))}
             </div>
-
-            <div className="mx-auto px-4 mt-10">
-
-                <div className="items-center flex flex-wrap">
-                    <div className="w-full hidden lg:block w-[450px] xl:w-[480px] ml-auto mr-auto px-2">
-                        <div className="flex flex-wrap gap-6">
-                            {[1, 2, 3, 4].map((service, index) => (
-                                <AnimatedCard key={`title${index}`} index={index} title={`service${service}`} icon={<BiLogoLinkedinSquare className="text-2xl text-white hover:text-yellow-600" />} />
-                            ))}
-                        </div>
-                    </div>
-                    <div className="w-full lg:w-6/12 ml-auto mr-auto">
-                        <div className="flex items-center justify-between mb-8 px-2 lg:px-0">
-                            <div className="style-circle rounded-lg bg-primary w-2.5 h-2.5"></div>
-                            <div className="style-circle rounded-lg bg-primary w-2.5 h-2.5"></div>
-                            <div className="style-line rounded-lg bg-primary h-[5px] w-[90%]"></div>
-                        </div>
-                        <div className="md:pr-12 px-4">
-
-                            <h3 className="text-3xl font-semibold">
-                                WHO AM I?
-                            </h3>
-                            <p className="mt-4 text-lg leading-relaxed text-gray-600">
-                                My name's Jane. I'm a web designer and developer based in Southampton, UK.
-
-                                During the day I work as a dfdfadjf klj ljalkd jdakda jkdla fjkdlfaj adk fjlakdfjkdjfkldjfkadjfkldjfkadjfkdjafkdjfalda kdjf kadj kd kladjf ldkjf adjf ldakjf ladfj a dfdfadjf klj ljalkd jdakda jkdla fjkdlfaj adk fjlakdfjkdjfkldjfkadjfkldjfkadjfkdjafkdjfalda kdjf kadj kd kladjf ldkjf adjf ldakjf ladfj a dfdfadjf klj ljalkd jdakda jkdla fjkdlfaj adk fjlakdfjkdjfkldjfkadjfkldjfkadjfkdjafkdjfalda kdjf kadj kd kladjf ldkjf adjf ldakjf ladfj lead developer at a local agency and in the evening I work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.
-                            </p>
-                            <ul className="list-none mt-6">
-                                <li className="py-2">
-                                    <BulletEllipse text="Carefully crafted components" textClasses="text-gray-600" />
-                                </li>
-                                <li className="py-2">
-                                    <BulletEllipse text="Amazing page examples" textClasses="text-gray-600" />
-                                </li>
-                                <li className="py-2">
-                                    <BulletEllipse text="Dynamic components" textClasses="text-gray-600" />
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+          </div>
+          <div className='w-full lg:w-6/12 ml-auto mr-auto'>
+            <div className='flex items-center justify-between mb-8 px-2 lg:px-0'>
+              <div className='style-circle rounded-lg bg-primary w-2.5 h-2.5'></div>
+              <div className='style-circle rounded-lg bg-primary w-2.5 h-2.5'></div>
+              <div className='style-line rounded-lg bg-primary h-[5px] w-[90%]'></div>
             </div>
-        </section>
-    );
+            <div className='md:pr-12 px-4'>
+              <h3 className='text-3xl font-semibold'>WHO AM I?</h3>
+              <p className='mt-4 text-lg leading-relaxed text-gray-600'>{portfolioData.description}</p>
+              <p className='mt-4 text-lg leading-relaxed text-gray-600'>Apart from coding, some other activities that I love to do!</p>
+              <ul className='list-none mt-6'>
+                <li className='py-2'>
+                  <BulletEllipse text='Reading' textClasses='text-gray-600' />
+                </li>
+                <li className='py-2'>
+                  <BulletEllipse text='Writing Tech Blogs' textClasses='text-gray-600' />
+                </li>
+                <li className='py-2'>
+                  <BulletEllipse text='Playing Games' textClasses='text-gray-600' />
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- frontend/src/pages/~portfolio/Landing/Landing.tsx
+++ frontend/src/pages/~portfolio/Landing/Landing.tsx
@@ -1,78 +1,62 @@
-import AuthorImage from "../../../assets/images/yat.jpg"
-import { BiLogoFacebookSquare, BiLogoTwitter, BiLogoInstagram, BiLogoLinkedinSquare, BiChevronRight } from "react-icons/bi";
+import { BiChevronRight } from 'react-icons/bi';
+import AuthorImage from '../../../assets/images/yat2.jpg';
+import { portfolioData } from '../../../data/portfolioData';
 const Landing = () => {
-
-    return (
-        <>
-            <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-[110vh] border-b border-gray-100"
-            >
-                <div className="absolute top-0 w-full h-full bg-center bg-cover"
-                    style={{
-                        backgroundImage: "url('/src/assets/images/bg-hero.jpg')"
-                    }}>
-                    <span id="blackOverlay" className="w-full h-full absolute opacity-75 bg-black"></span>
-                </div>
-                <div className="container relative mx-auto">
-                    <div className="items-center flex flex-wrap">
-                        <div className="w-full lg:w-10/12 px-4 ml-auto mr-auto text-center">
-                            <div
-                                className="container relative z-30  pb-12 sm:pt-44 sm:pb-48 lg:pt-54 lg:pb-48"
-                            >
-                                <div className="flex flex-col items-center justify-center lg:flex-row">
-                                    <div className="rounded-full border-8 border-white shadow-xl">
-                                        <img
-                                            src={AuthorImage}
-                                            className="h-48 rounded-full sm:h-56"
-                                            alt="author"
-                                        />
-                                    </div>
-                                    <div className="pt-8 sm:pt-10 lg:pl-8 lg:pt-0">
-                                        <h1
-                                            className="text-center font-header text-4xl text-white sm:text-left sm:text-5xl md:text-6xl"
-                                        >
-                                            Hello I'm Yathartha Varshneya!
-                                        </h1>
-                                        <div
-                                            className="flex flex-col justify-center pt-3 sm:flex-row sm:pt-5 lg:justify-start"
-                                        >
-                                            <div
-                                                className="flex items-center justify-center pl-0 sm:justify-start md:pl-1"
-                                            >
-                                                <p className="font-body text-lg uppercase text-white">Let's connect</p>
-                                                <div className="hidden sm:block">
-                                                    <BiChevronRight className="text-3xl text-yellow-600"></BiChevronRight>
-                                                </div>
-                                            </div>
-                                            <div
-                                                className="flex items-center justify-center pt-5 pl-2 sm:justify-start sm:pt-0"
-                                            >
-                                                <a href="/">
-                                                    <BiLogoFacebookSquare className="text-2xl text-white hover:text-yellow-600" />
-                                                </a>
-                                                <a href="/" className="pl-4">
-                                                <BiLogoTwitter className="text-2xl text-white hover:text-yellow-600" />
-                                                   
-                                                </a>
-                                                <a href="/" className="pl-4">
-                                              
-                                                <BiLogoLinkedinSquare className="text-2xl text-white hover:text-yellow-600" />
-                                                </a>
-                                                <a href="/" className="pl-4">
-                                                <BiLogoInstagram className="text-2xl text-white hover:text-yellow-600" />
-                                                 
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+  return (
+    <>
+      <div className='relative pt-16 pb-32 flex content-center items-center justify-center min-h-[110vh] border-b border-gray-100'>
+        <div
+          className='absolute top-0 w-full h-full bg-center bg-cover'
+          style={{
+            backgroundImage: "url('/src/assets/images/bg-hero.jpg')",
+          }}
+        >
+          <span id='blackOverlay' className='w-full h-full absolute opacity-75 bg-black'></span>
+        </div>
+        <div className='container relative mx-auto'>
+          <div className='items-center flex flex-wrap'>
+            <div className='w-full lg:w-10/12 px-4 ml-auto mr-auto text-center'>
+              <div className='container relative z-30  pb-12 sm:pt-44 sm:pb-48 lg:pt-54 lg:pb-48'>
+                <div className='flex flex-col items-center justify-center lg:flex-row'>
+                  <div className='rounded-full border-8 border-gray-300 shadow-xl'>
+                    <img src={AuthorImage} className='h-48 rounded-full sm:h-56' alt='author' />
+                  </div>
+                  <div className='pt-8 sm:pt-10 lg:pl-8 lg:pt-0'>
+                    <h1 className='text-center font-header text-4xl text-white sm:text-left sm:text-5xl md:text-6xl'>
+                      Hello I am Yathartha Varshneya!
+                    </h1>
+                    <div className='flex flex-col justify-center pt-3 sm:flex-row sm:pt-5 lg:justify-start'>
+                      <div className='flex items-center justify-center pl-0 sm:justify-start md:pl-1'>
+                        <p className='font-body text-lg uppercase text-white'>Let's connect</p>
+                        <div className='hidden sm:block'>
+                          <BiChevronRight className='text-3xl text-yellow-600'></BiChevronRight>
                         </div>
-
+                      </div>
+                      <div className='flex items-center justify-center pt-5 pl-2 sm:justify-start sm:pt-0'>
+                        {portfolioData?.publicProfiles?.map((profile, i) => {
+                          return (
+                            <a
+                              href={profile.link}
+                              className={`${i ? 'pl-4' : ''} text-2xl text-white hover:text-yellow-600`}
+                              target='_blank'
+                              rel='noopener noreferrer'
+                            >
+                              {profile.icon}
+                            </a>
+                          );
+                        })}
+                       
+                      </div>
                     </div>
+                  </div>
                 </div>
+              </div>
             </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Landing;
