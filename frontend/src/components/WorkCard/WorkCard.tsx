9cb52b93f9bd47adc1bd6c3009cf2a4bcbf16a81
Yathartha <varshneya.yathartha@gmail.com>
2024-10-25 14:14:23 +0530

    Updated work and achievemnent cards

--- frontend/src/components/WorkCard/WorkCard.tsx
+++ frontend/src/components/WorkCard/WorkCard.tsx
@@ -1,4 +1,3 @@
-
 export interface WorkCardProps {
   title: string;
   subTitle: string;
@@ -7,32 +6,22 @@ export interface WorkCardProps {
   children?: any;
 }
 
-const WorkCard = ({
-  title,
-  subTitle,
-  buttonText,
-  buttonHref,
-  children
-}: WorkCardProps) => {
- 
+const WorkCard = ({ title, subTitle, buttonText, buttonHref, children }: WorkCardProps) => {
   return (
     <>
-      <div
-        className="w-full px-12 md:w-1/2 xl:w-1/3 block"
-      >
-        <div className="relative mb-12">
-          <div className="overflow-hidden rounded-[10px] bg-gradient-to-b from-slate-400 to-slate-600 w-full h-[300px]">
+      <div className='w-full px-12 md:w-1/2 xl:w-1/3 block'>
+        <div className='relative mb-12'>
+          <div className='overflow-hidden rounded-[10px] bg-gradient-to-b from-slate-400 to-slate-600 w-full h-[300px]'>
             {children}
-            
           </div>
-          <div className="relative z-10 mx-7 -mt-20 rounded-lg bg-white dark:bg-dark-2 py-[34px] px-3 text-center shadow-portfolio dark:shadow-box-dark">
-            <h3 className="text-dark dark:text-white mb-2 text-xl font-bold">{title}</h3>
-            <span className="text-gray-600 mb-5 block text-sm font-medium">
-              {subTitle}
-            </span>
+          <div className='relative z-10 mx-7 -mt-20 rounded-lg bg-white dark:bg-dark-2 py-[34px] px-3 text-center shadow-portfolio dark:shadow-box-dark'>
+            <h3 className='text-dark dark:text-white mb-2 text-xl font-bold'>{title}</h3>
+            <span className='text-gray-600 mb-5 block text-sm font-medium'>{subTitle}</span>
             <a
               href={buttonHref}
-              className="text-body-color dark:text-dark-6 hover:border-slate-600 bg-slate-200 hover:bg-slate-600 inline-block rounded-md border border-stroke dark:border-dark-3 py-[10px] px-7 text-sm font-medium transition hover:text-white"
+              className='text-body-color dark:text-dark-6 hover:border-slate-600 bg-slate-200 hover:bg-slate-600 inline-block rounded-md border border-stroke dark:border-dark-3 py-[10px] px-7 text-sm font-medium transition hover:text-white'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               {buttonText}
             </a>
