84e2272805d4b778ebcfa0036be36885abaae886
Yathartha <varshneya.yathartha@gmail.com>
2024-11-02 19:50:36 +0530

    updated use case to work for multiple users

--- backend/my_app/api/routes/question_route.py
+++ backend/my_app/api/routes/question_route.py
@@ -1,11 +1,10 @@
 from typing import Annotated
-from fastapi import APIRouter, Body, Depends, Form, HTTPException, Request
-from my_app.common.models.question_dto import QueryTextDto
-from my_app.domain.repository_interfaces.file_repository import FileRepository
+from fastapi import APIRouter, Body, Depends, HTTPException, Request
+from my_app.common.models.question_dto import QueryDto, QueryTextDto
 from my_app.domain.repository_interfaces.storage_repository import StorageRepository
 from my_app.domain.usecases.genai_use_cases import generate_content_use_case, process_text_use_case, query_document_use_case
 from my_app.domain.repository_interfaces.genai_repository import GenaiRepository
-from my_app.api.dependencies import get_file_repository, get_genai_repository, get_storage_repository
+from my_app.api.dependencies import get_genai_repository, get_storage_repository
 from fastapi import APIRouter
 
 
@@ -19,26 +18,27 @@ def models(genai_repository: Annotated[GenaiRepository, Depends(get_genai_reposi
     
   
 @router.post("/processtext")
-def upload_files(article_dto: QueryTextDto, request: Request, genai_repository: Annotated[GenaiRepository, Depends(get_genai_repository)], storage_repository: Annotated[StorageRepository, Depends(get_storage_repository)], file_repository: Annotated[FileRepository, Depends(get_file_repository)]) -> str:
-    print(article_dto)
-    if article_dto.text_content:
+def upload_files(article_dto: QueryTextDto, request: Request, genai_repository: Annotated[GenaiRepository, Depends(get_genai_repository)], storage_repository: Annotated[StorageRepository, Depends(get_storage_repository)]) -> str:
+    if article_dto.email and article_dto.text_content:
         try:
-            process_text_use_case(article_dto.text_content, genai_repository, storage_repository, request.app.state.genai_key)
-        except:
+            process_text_use_case(article_dto.text_content, genai_repository, storage_repository, request.app.state.genai_key, article_dto.email)
+        except Exception as e:
+            print(e)
             raise HTTPException(status_code=500, detail="Error in processing text")
     else:
-        return 'Empty text'
-    return  'text processed successfully'
+        raise HTTPException(status_code=400, detail="Invalid input")
+    return  article_dto.email
 
 
 @router.post("/query", response_model=str)
-def query(query: Annotated[str, Body(embed=True)], request: Request, genai_repository: Annotated[GenaiRepository, Depends(get_genai_repository)], storage_repository: Annotated[StorageRepository, Depends(get_storage_repository)]) -> str :
-    if query:
+def query(query_dto: QueryDto, request: Request, genai_repository: Annotated[GenaiRepository, Depends(get_genai_repository)], storage_repository: Annotated[StorageRepository, Depends(get_storage_repository)]) -> str :
+    if query_dto.email and query_dto.query:
         try:
-            result = query_document_use_case(genai_repository, storage_repository, query, request.app.state.genai_key) 
+            result = query_document_use_case(genai_repository, storage_repository, query_dto.query, request.app.state.genai_key, query_dto.email) 
         
             return result
-        except:
+        except Exception as e:
+            print(e)
             raise HTTPException(status_code=500, detail="Error in querying document")
     
     return 'Empty question'
--- backend/my_app/domain/usecases/genai_use_cases.py
+++ backend/my_app/domain/usecases/genai_use_cases.py
@@ -1,6 +1,7 @@
 
 
 from typing import BinaryIO
+from my_app.common.util import get_blob_key
 from my_app.domain.repository_interfaces.file_repository import FileRepository
 from my_app.domain.repository_interfaces.storage_repository import StorageRepository
 from my_app.domain.repository_interfaces.genai_repository import GenaiRepository
@@ -13,28 +14,31 @@ def configure_genai_use_case(repo: GenaiRepository, key: str ) -> str:
 def generate_content_use_case(repo: GenaiRepository, prompt: str ) -> str:
     return repo.generate_content(prompt)
 
-def process_file_use_case(genai_repo: GenaiRepository, file_repo: FileRepository, storage_repo: StorageRepository , file: BinaryIO, key:str ):
+def process_file_use_case(genai_repo: GenaiRepository, file_repo: FileRepository, storage_repo: StorageRepository , file: BinaryIO, key:str, email: str  ):
     try:
         raw_text = file_repo.get_file_text(file)
-        process_text_use_case(raw_text, genai_repo, storage_repo, key)
+        process_text_use_case(raw_text, genai_repo, storage_repo, key, email)
         
     except Exception as e:
         # logger.error(e)
         raise e
     
-def process_text_use_case(text: str, genai_repo: GenaiRepository, storage_repo: StorageRepository, key:str ):
+def process_text_use_case(text: str, genai_repo: GenaiRepository, storage_repo: StorageRepository, key:str, email: str ):
     try:
         text_chunks = genai_repo.get_text_chunks(text)
         serialized_vector_store = genai_repo.get_serialized_vector_store(text_chunks, key)
-        storage_repo.upload_blob(settings.QUESTION_BUCKET, settings.BLOB_BASE_NAME, serialized_vector_store)
+        blob_key = get_blob_key(email)
+        storage_repo.upload_blob(settings.QUESTION_BUCKET, blob_key, serialized_vector_store)
         
     except Exception as e:
         # logger.error(e)
         raise e
     
-def query_document_use_case(genai_repo: GenaiRepository, storage_repo: StorageRepository , question: str, genai_key:str ) -> str:
+def query_document_use_case(genai_repo: GenaiRepository, storage_repo: StorageRepository , question: str, genai_key:str, email: str ) -> str:
     try:
-        serialized_vector_store = storage_repo.download_blob_into_memory(settings.QUESTION_BUCKET, settings.BLOB_BASE_NAME)
+        blob_key = get_blob_key(email)
+       
+        serialized_vector_store = storage_repo.download_blob_into_memory(settings.QUESTION_BUCKET, blob_key)
         return genai_repo.query_document(question, genai_key, serialized_vector_store) 
 
     except Exception as e:
