e91a507c89b58c4b5161c244407c75befdcbec03
Yathartha <varshneya.yathartha@gmail.com>
2025-07-17 22:10:12 +0530

    fixed deployment issues

--- backend/my_app/main.py
+++ backend/my_app/main.py
@@ -14,36 +14,28 @@ def custom_generate_unique_id(route: APIRoute) -> str:
 async def lifespan(app: FastAPI):
     get_key_and_configure_genai(app)
     yield
-    
 
 app = FastAPI(
     title=settings.APP_NAME,
     # openapi_url=f"{settings.API_V1_STR}/openapi.json",
     root_path=settings.ROOT_PATH,
-    lifespan=lifespan
+    lifespan=lifespan,
 )
 
 # Set all CORS enabled origins
-# if settings.BACKEND_CORS_ORIGINS:
-#     app.add_middleware(
-#         CORSMiddleware,
-#         allow_origins=[
-#             str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS
-#         ],
-#         allow_credentials=True,
-#         allow_methods=["*"],
-#         allow_headers=["*"],
-#     )
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS
+        ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 app.include_router(api_router)
 
 @app.get("/")
 def index():
-    return  'navigate to /docs route for open API spec'
-
-
-
-
-
-
-
+    return "navigate to /docs route for open API spec"
