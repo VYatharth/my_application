68fd0e2800e1260f99891804b92d3ceb8abe1865
Yathartha <varshneya.yathartha@gmail.com>
2025-10-15 03:13:08 +0530

    updated main file

--- backend/my_app/api/main.py
+++ backend/my_app/api/main.py
@@ -3,6 +3,6 @@ from my_app.api.routes import login, portfolio_route, question_route
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
-api_router.include_router(portfolio_route.router, prefix="/portfolio", tags=["portfolio"])
+api_router.include_router(portfolio_route.router)
 api_router.include_router(question_route.router, prefix="/question", tags=["question"])
 # api_router.include_router(scan_pdf_route.router, prefix="/pdf", tags=["pdf"])
